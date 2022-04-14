c04a80d1dac2ae1487287595fe4e6723bebf2e71
hle68 <86068557+hle68@users.noreply.github.com>
2022-04-14 12:10:39 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Are you sure you are in the right place? I bet not!
+  Welcome to ${PREFIX}'s app. TERRAFORMING IS FUN!!!
   <!-- END -->
   
   </div>
